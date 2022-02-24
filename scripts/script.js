22a229e889ac8b6c206528301a275fd912d4fae0
fvaldez29 <fvaldez29@hotmail.com>
2022-02-24 17:44:17 -0400

    Agregue el archivo JS al documento y realice un ejercicios de funciones tipos flechas con un ciclo for

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/*const mono = (base, exponente) => {
+    resultado = 1;
+    for(let numero = 0; numero < exponente; numero++){
+        resultado *= base;
+    }
+    return resultado;
+};
+document.write(mono(2, 5));*/
